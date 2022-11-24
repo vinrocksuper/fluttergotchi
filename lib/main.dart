d16f5032a93e0886ad88f521f65eacc083f90832
Vincent Li <35306435+vinrocksuper@users.noreply.github.com>
2022-11-24 17:09:30 -0500

    decay over time working, saving, loading working

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:project3/my_flutter_app_icons.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -57,13 +58,15 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   // Misc Tools
   Random rng = Random();
-  bool debug = false;
+  bool debug = true;
   bool isAlive = true;
   bool isAsleep = false;
+  late SharedPreferences prefs;
 
   @override
   void initState() {
     super.initState();
+    init();
     WidgetsBinding.instance.addObserver(this);
     timer = Timer.periodic(Duration(seconds: 30), (timer) {
       setState(() {
@@ -79,10 +82,30 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
           ageUp();
         }
         if (timeUntilNextFeeding <= 0) {
+          timeUntilNextFeeding = 0;
           canEat = true;
         }
       });
     });
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    switch (state) {
+      case AppLifecycleState.resumed:
+        loadStats();
+        break;
+      case AppLifecycleState.inactive:
+        saveStats();
+        break;
+      case AppLifecycleState.paused:
+        saveStats();
+        break;
+      default: // detached
+        saveStats();
+        break;
+    }
   }
 
   @override
@@ -97,6 +120,89 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       age++;
       timeUntilNextAge = 3600;
     });
+  }
+
+  void init() async {
+    prefs = await SharedPreferences.getInstance();
+    loadStats();
+  }
+
+  void loadStats() {
+    setState(() {
+      int? hun = prefs.getInt('hunger');
+      if (hun != null) {
+        hunger = hun;
+      }
+      int? c = prefs.getInt('cleanliness');
+      if (c != null) {
+        cleanliness = c;
+      }
+      int? en = prefs.getInt('energy');
+      if (en != null) {
+        energy = en;
+      }
+      int? hap = prefs.getInt('happiness');
+      if (hap != null) {
+        happiness = hap;
+      }
+      int? hea = prefs.getInt('health');
+      if (hea != null) {
+        health = hea;
+      }
+
+      final lastOpened = prefs.getInt('lastOpened');
+      final now = (DateTime.now().millisecondsSinceEpoch / 1000).floor();
+      if (lastOpened != null) {
+        final elapsedTime = now - lastOpened;
+
+        // Decay (or increase) stats
+        int toRun = (elapsedTime / 60 / 4).floor();
+        hunger -= toRun;
+        cleanliness -= toRun;
+
+        final wasAsleep = prefs.getBool('wasSleeping');
+        if (wasAsleep!) {
+          energy += (toRun / 2).floor();
+          happiness += (toRun / 4).floor();
+        } else {
+          energy -= toRun;
+          happiness -= toRun;
+        }
+
+        adjustHealth(multiplier: (toRun / 4).floor());
+
+        // Age up
+        final toNext = prefs.getInt('timeToNextAge');
+        if (toNext != null) {
+          final toAge = (elapsedTime / 3600).floor();
+          final toNextAge = (elapsedTime % 3600);
+          age = toAge;
+          timeUntilNextAge = toNextAge;
+        }
+
+        final nextFeeding = prefs.getInt('timeNextEat');
+        if (nextFeeding != null) {
+          timeUntilNextFeeding = nextFeeding - elapsedTime;
+        }
+        canEat = prefs.getBool('canEat')!;
+      }
+    });
+  }
+
+  void saveStats() {
+    // Sets time last opened to compare so can decay stats
+    final now = ((DateTime.now()).millisecondsSinceEpoch / 1000).floor();
+    prefs.setInt('lastOpened', now);
+    prefs.setBool('wasSleeping', isAsleep);
+    prefs.setInt('age', age);
+    prefs.setInt('timeToNextAge', timeUntilNextAge);
+    prefs.setInt('hunger', hunger);
+    prefs.setInt('cleanliness', cleanliness);
+    prefs.setInt('energy', energy);
+    prefs.setInt('happiness', happiness);
+    prefs.setInt('health', health);
+    prefs.setBool('canEat', canEat);
+    prefs.setInt('timeNextEat', timeUntilNextFeeding);
   }
 
   /// Technically a misnomer since it also increases stats if the fluttergotchi is asleep
@@ -208,10 +314,19 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
               visible: debug,
               child: ElevatedButton(
                 onPressed: () {
-                  hunger = 50;
-                  cleanliness = 50;
-                  energy = 50;
-                  happiness = 50;
+                  hunger = 90;
+                  cleanliness = 60;
+                  energy = 70;
+                  happiness = 40;
+                  print(hunger);
+                  print(cleanliness);
+                  print(energy);
+                  print(happiness);
+                  print(health);
+                  print(age);
+                  print(timeUntilNextAge);
+                  print(canEat);
+                  print(timeUntilNextFeeding);
                 },
                 child: Text('Debug Reset'),
               ),
@@ -363,6 +478,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
         ),
         ElevatedButton.icon(
           onPressed: () {
+            print('${canEat} bread');
             if (canEat) {
               adjustStat(toAdjust: 'hunger', amount: 50);
               setState(() {
@@ -417,44 +533,44 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     }
   }
 
-  void adjustHealth() {
+  void adjustHealth({multiplier = 1}) {
     setState(() {
       // Become sick if hungry/dirty/tired/depressed
       if (hunger <= 0) {
-        health -= 2;
+        health -= (2 * multiplier) as int;
       }
 
       if (cleanliness <= 33) {
-        health -= 2;
+        health -= (2 * multiplier) as int;
       } else if (cleanliness <= 66) {
-        health--;
+        health -= (1 * multiplier) as int;
       }
 
       if (energy <= 20) {
-        health--;
+        health -= (1 * multiplier) as int;
       }
 
       if (happiness < 33) {
-        health--;
+        health -= (1 * multiplier) as int;
       }
 
       // Alternatively become healthier if the opposite
       if (hunger >= 85) {
-        health += 2;
+        health += (2 * multiplier) as int;
       } else if (hunger > 66) {
-        health++;
+        health += (1 * multiplier) as int;
       }
 
       if (cleanliness >= 75) {
-        health++;
+        health += (1 * multiplier) as int;
       }
 
       if (energy > 50) {
-        health++;
+        health += (1 * multiplier) as int;
       }
 
       if (happiness > 75) {
-        health++;
+        health += (1 * multiplier) as int;
       }
     });
   }
@@ -623,59 +739,62 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
             height: 15,
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Age",
-                  style: TextStyle(
-                    fontSize: 16,
+        Visibility(
+          visible: !debug,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Age",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Text(
-                  age.toString(),
-                  style: TextStyle(
-                    fontSize: 24,
+                  SizedBox(
+                    height: 25,
                   ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 7.5,
-              child: VerticalDivider(
-                width: 6,
-                thickness: 6,
-                indent: 20,
-                endIndent: 0,
-                color: Colors.black12,
+                  Text(
+                    age.toString(),
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  )
+                ],
               ),
-            ),
-            Column(
-              children: [
-                Text(
-                  "Weight",
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 7.5,
+                child: VerticalDivider(
+                  width: 6,
+                  thickness: 6,
+                  indent: 20,
+                  endIndent: 0,
+                  color: Colors.black12,
                 ),
-                SizedBox(
-                  height: 25,
-                ),
-                Text(
-                  weight.toString(),
-                  style: TextStyle(
-                    fontSize: 24,
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Weight",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
                   ),
-                )
-              ],
-            ),
-          ],
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text(
+                    weight.toString(),
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         )
       ],
     );
